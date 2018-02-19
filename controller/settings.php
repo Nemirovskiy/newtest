a357d466dcfacbe5511bea7461b027cdc62942a8
Николай <it-nikola@mail.ru>
2018-02-19 16:26:12 +0300

    добавлены файлы ошибок 404 и 500

--- controller/settings.php
+++ controller/settings.php
@@ -4,6 +4,7 @@
 define('VIEW_DIR_PAGE', 'view/page/');
 define('VIEW_DIR_TEST', 'view/test/');
 define('VIEW_DIR_INCLUDE', 'view/include/');
+define('VIEW_DIR_ERORS', 'view/errors/');
 /// пути классов
 define('CLASS_DIR_CONTROLLER', 'controller/');
 define('CLASS_DIR_MODEL', 'model/');
