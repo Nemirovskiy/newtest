616c3ddf5863791f278b6634237ed8fc9c6b7eff
Николай <it-nikola@mail.ru>
2018-02-23 08:11:37 +0300

    убрана отладка из класса тест

--- model/Test.class.php
+++ model/Test.class.php
@@ -15,29 +15,32 @@ class Test extends Page
         $random = false; // будем получать из метода получения пользовательских настроек
         $arr = $this->getTest($testTheme,$number,$random);
         echo "<pre>";
-//        print_r($arr);
+        print_r($arr);
         echo "</pre>";
-        $theme = $arr['theme'];
-        $theme = $arr['quest'];
-        $code = $testTheme;
-        $num = $arr['number'];
-        $quest = $arr['quest'];
-        $answers = $arr['answers'];
-        include VIEW_DIR_TEST.'test.php';
+        if(empty($arr)){
+            include VIEW_DIR_TEST.'notest.php';
+        }
+        else{
+            $theme = $arr['theme'];
+            $code = $testTheme;
+            $num = $arr['number'];
+            $quest = $arr['quest'];
+            $answers = $arr['answers'];
+            include VIEW_DIR_TEST.'test.php';
+        }
+        include VIEW_DIR_TEST.'footer.php';
     }
     protected function getTest($theme,$number,$rnd = false){
         // метод получения вопроса и ответа из указанной темы и указанного номера вопроса
         $query = "SELECT * from quest INNER JOIN theme ON (quest.theme_code = theme.theme_code)
                   INNER JOIN answ ON (quest.quest_id = answ.quest_id) WHERE theme.theme_code = ? AND quest_number = ?";
         $tests = DBase::select($query,[$theme,$number]);
-        echo "<pre>";
-//        print_r($tests);
-        echo "</pre>";
+        if(empty($tests)) return false;
         $result =[];
         foreach ($tests as $key => $item){
             if(!isset($result['number'])) $result['number'] = $item['quest_number'];
             if(!isset($result['quest'])) $result['quest'] = $item['quest_text'];
-            if(!isset($result['theme'])) $result['quest'] = $item['theme_text'];
+            if(!isset($result['theme'])) $result['theme'] = $item['theme_text'];
             $result['answers'][] = [
                 'order'=> $item['answ_order'],
                 'right' => $item['answ_right'],
