075010bfe66b4486d9f9da03ecd8cb5c9a8cc8c7
Николай <it-nikola@mail.ru>
2018-02-23 08:10:46 +0300

    добавление папки представлений администратора

--- controller/settings.php
+++ controller/settings.php
@@ -2,6 +2,7 @@
 // константы путей
 /// пути представления
 define('VIEW_DIR_PAGE', 'view/page/');
+define('VIEW_DIR_ADMIN', 'view/admin/');
 define('VIEW_DIR_TEST', 'view/test/');
 define('VIEW_DIR_INCLUDE', 'view/include/');
 define('VIEW_DIR_ERORS', 'view/errors/');
