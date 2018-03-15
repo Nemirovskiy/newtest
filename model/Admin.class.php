08cd020db8c63e68c91fccb3a16aa508667bdb15
Николай <it-nikola@mail.ru>
2018-03-16 00:03:39 +0300

    класс админ. методы для добавления тестов - перевод с процедурного на ООП, добавление комментариев

--- model/Admin.class.php
+++ model/Admin.class.php
@@ -1,103 +1,156 @@
 <?
 /**
-* модель страницы
- * - заголовок
- * - символьное имя - адрес
- * - меню
- * - контент
- * + метод получения данных
- * + метод подготовки заголовка
- * + метод подготовки основного контента
- * + метод формирования страницы
-*/
+ * Класс Администратора
+ *
+ */
+
 class Admin extends Page
 {
-	/*protected $title='Тестирование - Главная'; // заголовок
-	private $nav=[];	 // навигация
-	private $code='';	 // символьное имя - код страницы
-    private static $list= null;	 // список всех страниц
-
-    private static function getList()
-    {
-        if(self::$list === null){
-            // получаем список страниц из БД
-            $arr = DBase::select("SELECT * FROM page ORDER BY menu");
-            self::$list = $arr;
-        }
-        return self::$list;
+    /**
+     * @var string $regexQuest - регулярное выражение для поиска вопроса
+     * @var string $regexAnsv - регулярное выражение для поиска ответа
+     * @var string $regexRight - регулярное выражение для поиска правильного ответа
+     */
+    protected static $regexQuest = "#^[\s|\t]*[\#|№]+[\s|\t]*([0-9]*)(.*)$#u";
+    protected static $regexAnsv = "#([а-яА-ЯёЁ\s]+)\)(.+)#u";
+    protected static $regexRight = "#([0-9]+)[\s\t]*([[а-яА-Яa-zA-Z|,\s]{1,10})$#u";
+    /**
+     * функция преобразования букв в порядковые номера
+     * @param string $liter - входящий символ или строка
+     * @return string  - преобразованый символ или строка
+     */
+    protected static function literToNum($liter=''){
+        if(is_numeric($liter)) return $liter;
+        $cirilic = explode(' ',"а б в г д е ж з и к л м н о");
+        $search  = array_merge($cirilic,range("a","n"));
+        // для замены готовим двойной массив из цифр
+        $replace = range(1,count($cirilic));
+        $replace = array_merge($replace,$replace);
+        $strlower = mb_strtolower($liter, mb_detect_encoding($liter));
+        return str_replace($search, $replace, trim($strlower));
     }
-	// метод формирования меню
-	private function setMenu()
-	{
-	    // получаем список страниц
-		$pages = self::getList();
-		$code = $this->code;
-		$nav = [];
-		$flag = false;
-		foreach ($pages as $key => $value) {
-		    if($value['menu'] > 0) {
-		        $nav[$key] = $value;
-		        if($value['code'] == $code){
-                     $nav[$key]['active'] = $flag= true;
+
+    /**
+     * Функция обработки текста в массив вопросов
+     * @param array $arrayStr входящий массив строк
+     * @return array $quests - возвращаемый массив вопросов
+     *
+     */
+    protected function convertQuests($arrayStr){
+        /**
+         * @var array $quests - возвращаемый массив вопросов
+         * @var int $quests_num -  номер текущего вопроса
+         */
+        $quests_num = '';
+        $quests =[];
+        $countQuest = 0;
+        $countRight = 0;
+        foreach($arrayStr as $string){
+            if (preg_match(self::$regexQuest, $string, $q)) //чтение вопроса
+            {
+                $quests_num = (int)$q[1];
+                // очищаем от лишних символов и убираем перевод строк
+                $quests[$quests_num]['quest'] = str_replace(["\n","\r"],"",trim($q[2],"\x00..\x2F \x3A..\x3B"));
+                $quests[$quests_num]['number'] = $quests_num;
+            }
+            elseif (preg_match(self::$regexAnsv, $string, $a)) //чтение вариантов ответа
+            {
+                $order = self::literToNum($a[1]);
+                $text = trim($a[2],".:;\s\t ");
+                $quests[$quests_num]['answers'][$order] = ['order' => $order, 'right'=>0,
+                    // очищаем от лишних символов и убираем перевод строк
+                    'text'=>str_replace(["\n","\r"],"",trim($a[2],"\x00..\x20 \x3A..\x3B"))];
+            }
+            if (preg_match(self::$regexRight, $string, $a)) //чтение правильного ответа
+            {
+                // если ответы написаны через пробел или запятую
+                $str = preg_split("#[,\s]*#u", $a[2]);
+                foreach ($str as $item){
+                    if($item) {
+                        $right = self::literToNum($item);
+                        // если ответы написаны слитно
+                        if(strlen($right) >1) {
+                            for($i = 0;$i<strlen($right);$i++){
+                                //echo $a[1]. " - " .$right[$i]."<br>";
+                                $quests[$a[1]]['answers'][$right[$i]]['right'] = 1;
+                            }
+                        }
+                        else {
+                            $quests[$a[1]]['answers'][$right]['right'] = 1;
+                        }
+                    }
                 }
-                // если код текущей старницы index - ссылка будет без кода
-                if($nav[$key]['code'] == 'index') $nav[$key]['code']='';
-            }
-		}
-        $this->nav = $nav;
-	}
-	// метод возвращения заголовка
-	private function setTitle()
-	{
-	    // получаем список страниц
-		$pages = self::getList();
-		$code = $this->code;
-		foreach ($pages as $item){
-		    if($item['code'] == $code){
-                $this->title = $item['title'];
-		        return;
             }
         }
-	}
-	private function validAdress($code){
-        foreach (self::getList() as $page){
-            if($code == $page['code'] || $code == 'index'){
-                return true;
-            }
-        }
-        header("HTTP/1.0 404 Not Found");
-        $this->title = "Нет такой страницы";
-        return false;
+        return $quests;
     }
-    protected function prepareHead($code)
+
+    /**
+     * Функция подготовки полученного текста
+     * в массив для дальнейшей переработки
+     * @return array $test - массив необработанных строк (вопросы, ответы)
+     */
+    protected function prepareQuests(){
+        $test = null;
+        if(!empty($_FILES['file']['tmp_name']))
+            $test = file($_FILES['file']['tmp_name']);
+            // проверка кодировки и преобразование
+            //mb_detect_order("CP1251,ASCI,UTF-7,UTF-8");
+            //print_r($test);
+            //$test2 = array_map('mb_convert_encoding',$test,
+                    //array_fill(0,count($test)-1,'UTF-8')/*,
+                    //array_fill(0,count($test)-1,"auto")*/);
+            //echo "<hr>".$test[0].$text[1].$test[2]."<br>";
+            //echo mb_detect_encoding($test[0].$text[1].$test[2])."<br>";
+            //print_r($test2);
+        if(!empty($_POST['text']))
+            $test = explode("\n",$_POST['text']);
+        return $test;
+    }
+
+    /**
+     * @param string $template
+     * @var array $arrayTests
+     */
+    protected function prepareBody($template)
 	{
-        $this->code = $code;
-        $this->setMenu();
-        $this->setTitle();
-        $nav = $this->nav;
-        $title = $this->title;
-        include VIEW_DIR_INCLUDE.'head.php';
-	}
-    */protected function prepareBody($template)
-	{
-		    include VIEW_DIR_ADMIN.$template.'.php';
+        /**
+         * 1. Формирование массива вопросов - ответов
+         *
+         * @var array $arrayTests
+         */
+	    if(empty($this->prepareQuests())){
+            include VIEW_DIR_ADMIN.$template.'.php';
+        }
+        else{
+            // вывод на страницу результат
+            $arrayTests = $this->convertQuests($this->prepareQuests());
+            foreach ($arrayTests as $arr){
+                $theme = $arr['theme'];
+                $code = 'Theme';
+                $num = $arr['number'];
+                $quest = $arr['quest'];
+                $answers = $arr['answers'];
+                include VIEW_DIR_ADMIN.'test_preview.php';
+            }
+        }
 	}
 
-	public function renderHtml($code)
-	{
-	    // проверяем на корректность адрес
-	    $valid = $this->validAdress($code);
-	    // подготавливаем голову
-        $this->prepareHead($code);
-        // если адрес корректный - подготовим контент
-        if($valid){
-            $this->prepareBody($code);
-        }
-        // иначе - отобразим старницу ошибки
-        else {
-            include VIEW_DIR_ERORS.'404.php';
-        }
-        // подключим низ страницы
-        include VIEW_DIR_INCLUDE.'footer.php';
-	}
+//	public function renderHtml($code)
+//	{
+//	    // проверяем на корректность адрес
+//	    $valid = $this->validAdress($code);
+//	    // подготавливаем голову
+//        $this->prepareHead($code);
+//        // если адрес корректный - подготовим контент
+//        if($valid){
+//            $this->prepareBody($code);
+//        }
+//        // иначе - отобразим старницу ошибки
+//        else {
+//            include VIEW_DIR_ERORS.'404.php';
+//        }
+//        // подключим низ страницы
+//        include VIEW_DIR_INCLUDE.'footer.php';
+//	}
 }
--- view/admin/admin.php
+++ view/admin/admin.php
@@ -1,31 +1,55 @@
 <?
-$test = file($_FILES['file']['tmp_name']);
-$i=0;
-$quests =[];
+/***************
+ */
+if(!empty($_FILES['file']['tmp_name']))
+    $test = file($_FILES['file']['tmp_name']);
+// проверка кодировки и преобразование
+//mb_detect_order("CP1251,ASCI,UTF-7,UTF-8");
+//print_r($test);
+//$test2 = array_map('mb_convert_encoding',$test,
+    //array_fill(0,count($test)-1,'UTF-8')/*,
+    //array_fill(0,count($test)-1,"auto")*/);
+//echo "<hr>".$test[0].$text[1].$test[2]."<br>";
+//echo mb_detect_encoding($test[0].$text[1].$test[2])."<br>";
+//print_r($test2);
+if(!empty($_POST['text']))
+    $test = explode("\n",$_POST['text']);
+/****
+ */
 // функция преобразования букв в порядковые номера
-function literToNum($liter=''){
+/*function literToNum($liter=''){
     if(is_numeric($liter)) return $liter;
     $cirilic = explode(' ',"а б в г д е ж з и к л м н о");
     $search  = array_merge($cirilic,range("a","n"));
+    // для замены готовим двойной массив из цифр
     $replace = range(1,count($cirilic));
     $replace = array_merge($replace,$replace);
     $strlower = mb_strtolower($liter, mb_detect_encoding($liter));
     return str_replace($search, $replace, trim($strlower));
 }
-$quests_num = '';
+
+// обработка текста в массив вопросов
+$quests_num = ''; // номер текущего вопроса
+$i=0;
+$quests =[];
+$countQuest = 0;
+$countRight = 0;
 foreach($test as $v){
-    if (preg_match("#[\s|\t]*[\#|№]+[\s|\t]*([0-9]*)(.*)#u", $v, $q)) //чтение вопроса
+    if (preg_match("#^[\s|\t]*[\#|№]+[\s|\t]*([0-9]*)(.*)$#u", $v, $q)) //чтение вопроса
     {
         $quests_num = (int)$q[1];
-        $quests_text = trim(trim($q[2],'. : ; 	'),'. : ; 	');
-        $quests[$quests_num]['quest'] = $quests_text;
+        // очищаем от лишних символов и убираем перевод строк
+        $quests[$quests_num]['quest'] = str_replace(["\n","\r"],"",trim($q[2],"\x00..\x2F \x3A..\x3B"));
         $quests[$quests_num]['number'] = $quests_num;
     }
-    elseif (preg_match("#([а-яА-ЯёЁ\s]+)\)(.*)#u", $v, $a)) //чтение вариантов ответа
+    elseif (preg_match("#([а-яА-ЯёЁ\s]+)\)(.+)#u", $v, $a)) //чтение вариантов ответа
     {
         $order = literToNum($a[1]);
-        $text = trim(trim($a[2]," . : ; ")," . : ; ");
-        $quests[$quests_num]['answers'][$order]= ['order' => $order, 'right'=>0, 'text'=>$text];
+        $text = trim($a[2],".:;\s\t ");
+        //$text = str_replace(["\n","\r"],"",trim($a[2],"\x00..\x20 \x3A..\x3B"));
+        $quests[$quests_num]['answers'][$order]= ['order' => $order, 'right'=>0,
+            // очищаем от лишних символов и убираем перевод строк
+            'text'=>str_replace(["\n","\r"],"",trim($a[2],"\x00..\x20 \x3A..\x3B"))];
     }
     if (preg_match("#([0-9]+)[\s\t]*([[а-яА-Яa-zA-Z|,\s]{1,10})$#u", $v, $a)) //чтение правильного ответа
     {
@@ -41,16 +65,15 @@ foreach($test as $v){
                         $quests[$a[1]]['answers'][$right[$i]]['right'] = 1;
                     }
                 }
-
                 else {
                     $quests[$a[1]]['answers'][$right]['right'] = 1;
                 }
             }
         }
     }
-
 }
 
+// вывод на страницу результат
 foreach ($quests as $arr){
     $theme = $arr['theme'];
     $code = 'Theme';
@@ -59,11 +82,42 @@ foreach ($quests as $arr){
     $answers = $arr['answers'];
     include VIEW_DIR_ADMIN.'test_preview.php';
 }
+
+//$stringQuest = "INSERT INTO `quest` (`theme_code`, `quest_number`, `quest_text`) VALUES";
+//$stringAnsw = "INSERT INTO `answ` (`quest_id`, `answ_order`, `answ_right`, `answ_text`) VALUES ";
+foreach ($quests as $arr){
+    $code = 'Theme';
+    $num = $arr['number'];
+    ////////////
+    /// INSERT INTO `answ` (`quest_id`, `answ_order`, `answ_right`, `answ_text`) VALUES
+    /// (1, 1, 0, 'Правильное чередование +зубцов Р, нормальных QRS с ЧСС 40-60 в 1 мин'),
+    ///
+
+    foreach($arr['answers'] as $key => $answer){
+        $stringAnsw[] = [$num,$key,$answer['right'],"'".addslashes($answer['text'])."'"];
+    }
+
+    ////////////
+    /// INSERT INTO `quest` (`theme_code`, `quest_number`, `quest_text`) VALUES
+    /// ('feld', 2, 'ИМПУЛЬС, ВЫШЕДШИЙ ИЗ А/В УЗЛА, ВЫГЛЯДИТ НА ЭКГ КАК'),
+    ///
+    $stringQuest[] = [$code,$num,"'".addslashes($arr['quest'])."'"];
+}
+// возвращаемое значение - строка для записи в БД -  неподготовленные строки
+echo "<pre>";
+print_r($stringAnsw);
+print_r($stringQuest);
+//echo rtrim($stringAnsw,", ")."<hr>";
+//echo rtrim($stringQuest,", ");
+echo "</pre>";
+
+*/
 ?>
 <div class='cntr'>
 <h1>Администратор</h1>
     <form method="POST" enctype="multipart/form-data">
         <input type="file" name="file">
+        <textarea name="text"></textarea>
         <input type="submit" value="отправить">
     </form>
 </div>
