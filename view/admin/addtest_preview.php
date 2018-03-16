2885c160af62bbef59f7d75a7fe8cc5912efa938
Николай <it-nikola@mail.ru>
2018-03-17 00:30:25 +0300

    перенос обработки массива вопросов в представление

---
+++ view/admin/addtest_preview.php
@@ -0,0 +1,20 @@
+<h3>
+    Добавляем тесты в тему <span style="border: 2px solid #00c;"><?=$theme['code']?> - <?=$theme['name']?></span>
+</h3>
+<? if(!empty($errors)): ?>
+<h4 style="color: red;"><?=$errors?></h4>
+<?endif;?>
+<ul>
+    <?foreach ($tests as $quest):?>
+    <li class="quest"><b class="quest-number">№ <?=$quest['number']?></b> <span class="quest-text"><?=$quest['quest']?></span></li>
+        <? foreach($quest['answers'] as $answer): ?>
+        <li class="<?if($answer['right']) echo "r_otv"?>" >
+            <?=$answer['text']?>
+        </li>
+        <? endforeach;?>
+    <? endforeach; ?>
+</ul>
+<form method="POST">
+    <button name="submit">Будем добавлять</button>
+    <button name="reset">Изменить</button>
+</form>
--- view/admin/test_preview.php
+++
@@ -1,8 +0,0 @@
-<h4><span>№ <?=$num?> </span><?=$quest?></h4>
-<ul>
-    <? foreach($answers as $answer): ?>
-    <li class="<?if($answer['right']) echo "r_otv"?>" >
-        <?=$answer['text']?>
-    </li>
-    <? endforeach; ?>
-</ul>
