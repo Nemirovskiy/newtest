1846641b09fcb1442235815b987b45f0b531066e
Николай <it-nikola@mail.ru>
2018-03-17 00:35:45 +0300

    добавления страницы авторизации

---
+++ view/page/login.php
@@ -0,0 +1,12 @@
+<div class='cntr'>
+    <h2>Вход только по пропуску</h2>
+    <form method="POST">
+        <input autofocus type="text" name="login" placeholder="login/email">
+        <br>
+        <input type="password" name="pass" placeholder="password">
+        <br>
+        <button>Войти</button>
+        <br>
+        <a href="#">Регистрация</a>
+    </form>
+</div>
