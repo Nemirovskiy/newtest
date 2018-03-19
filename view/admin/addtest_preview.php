0aa898c8a6e529d15a8098db35a0d95886a2be6b
Николай <it-nikola@mail.ru>
2018-03-19 22:34:08 +0300

    добавление вывода сообщений, отображение кнопки добавить только  если нет ошибок

--- view/admin/addtest_preview.php
+++ view/admin/addtest_preview.php
@@ -2,21 +2,26 @@
     Добавляем тесты в тему <span style="border: 2px solid #00c;"><?=$theme['code']?> - <?=$theme['name']?></span>
 </h3>
 <? if(!empty($errors)): ?>
-<h4 style="color: red;"><?=$errors?></h4>
+    <h4 style="color: red;"><?=$errors?></h4>
+<?endif;?>
+<? if(!empty($message)): ?>
+    <h4 style="color: blue;"><?=$message?></h4>
 <?endif;?>
 <ul>
     <?foreach ($tests as $quest):?>
-    <li class="quest">
-        <b class="quest-number">№ <?=$quest['number']?></b>
-        <span class="quest-text"><?=$quest['quest']?></span></li>
+        <li class="quest">
+            <b class="quest-number">№ <?=$quest['number']?></b>
+            <span class="quest-text"><?=$quest['quest']?></span></li>
         <? foreach($quest['answers'] as $answer): ?>
-        <li class="<?if($answer['right']) echo "r_otv"?>" >
-            <?=$answer['text']?>
-        </li>
+            <li class="<?if($answer['right']) echo "r_otv"?>" >
+                <?=$answer['text']?>
+            </li>
         <? endforeach;?>
     <? endforeach; ?>
 </ul>
 <form method="POST">
-    <button name="submit">Будем добавлять</button>
+    <? if(empty($errors)): ?>
+        <button name="submit">Будем добавлять</button>
+    <?endif;?>
     <button name="reset">Изменить</button>
 </form>
