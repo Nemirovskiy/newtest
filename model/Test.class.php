a14f7b76f62bbf2414555050161567bf8b8048d4
Николай <it-nikola@mail.ru>
2018-03-22 20:04:22 +0300

    переделка логики кода формирования контента страницы, +метод определения состояний (separation) - формирующий название состояния, +методы формирования контента по состоянию

--- model/Test.class.php
+++ model/Test.class.php
@@ -4,64 +4,139 @@
 */
 class Test extends Page
 {
-	public static $list    = null; 	// Список тем
-	public static $theme   = []  ; 	// Тема
-	public static $num     = ''  ; 	// номер вопроса
-	public static $quest   = ''  ; 	// Текст вопроса
-	public static $answers = []  ; 	// ответы
+	public static  $list    = null; 	// Список тем
+	public static  $theme   = []  ; 	// Тема
+	public static  $num     = ''  ; 	// номер вопроса
+	public static  $quest   = ''  ; 	// Текст вопроса
+	public static  $answers = []  ; 	// ответы
 
     private function checkAnswer($theme){
+        /**
+         * $_SESSION['used'][$theme][]  - массив отвеченных вопросов в теме
+         * $_SESSION['right'][$theme][] - массив правильных ответов в теме
+         */
             $answer = $_POST[$theme];
             $right = $_SESSION["curent"]["right"];
             foreach ($answer as $item){
                 $_SESSION["curent"]["answers"][$item - 1]['check'] = true;
             }
-
-            echo "<br>отвечено: ".implode(",",$answer);
-            echo "<br>надо: ".implode(",",$right);
-            echo "<hr>";
-            if(implode(",",$answer) === implode(",",$right))
+            $_SESSION['used'][$theme][] = $_SESSION['curent']['number'];
+            if(implode(",",$answer) === implode(",",$right)){
+                $_SESSION['right'][$theme][] = $_SESSION['curent']['number'];
                 return true;
+            }
             else
                 return false;
     }
+
     /**
-     * Метод подготовки основгой части страницы
+     * метод генерации номера вопроса без совпадения
+     * @param bool $infinity
+     * @return int
+     */
+    private function generateNumberQuest($infinity = false){
+        $theme = $this->code;
+        if($infinity || empty($_SESSION['used'][$theme]))
+            $number = rand(1,self::getThemeList()[$theme]['count']);
+        elseif(count($_SESSION['used'][$theme]) < self::getThemeList()[$theme]['count']){
+            do{
+                $number = rand(1,self::getThemeList()[$theme]['count']);
+            }while(in_array($number,$_SESSION['used'][$theme]));
+        }
+        else
+            $number = false;
+        return $number;
+    }
+
+    /**
+     * @return string метод оформления
+     *
+     */
+    private function separation(){
+        /**
+         *
+         */
+        // код подготовки выражений для проверки
+        $theme = $this->code;
+        $count = self::getThemeList()[$theme]['count'];
+        $answer = !empty($_POST[$theme]);
+
+        if($count < 1){
+            // вопросов в теме ещё нет
+            return 'notest';
+        }
+        elseif($answer){
+            // если есть ответ
+            if($this->checkAnswer($theme)){
+                // ответ верный
+                // показать, что ответ верный
+                $this->message = 'right';
+            }
+            else{
+                // ответ не верный
+                // показать ответ
+                $this->message = 'Не верно!';
+                return 'wrong';
+            }
+        }
+        if(count($_SESSION['used'][$theme]) == $count){
+            // отвечены все вопросы / ответ верный и вопросов больше нет
+            // показать статистику
+            return 'result';
+        }
+        // показать новый вопрос
+        return 'test';
+    }
+    private function buildTest(){
+        $_SESSION['curent']['code'] = $code = $this->code;
+        $test = $this->getTest($code,$this->generateNumberQuest());
+        $_SESSION['curent'] = $test;
+        $test['code'] = $code;
+        $test['right'] = '';
+        $test['message'] = $this->message;
+        $test['errors'] = $this->errors;
+        return $test;
+    }
+    private function buildWrong(){
+        $result = $_SESSION['curent'];
+        $result['message'] = $this->message;
+        $result['errors'] = $this->errors;
+        return $result;
+    }
+
+    private function buildResult(){
+        $code = $this->code;
+        //$result = $_SESSION;
+        //$_SESSION["curent"]["code"] = $code = $testTheme;
+        $result['used'] = $u = count($_SESSION['used'][$code]);
+        $result['right'] = $r = count($_SESSION['right'][$code]);
+        $result['ratio'] = ($r>0) ? round($r / $u * 100) : 0 ;
+        //$result['answers'] = $_SESSION["curent"]["answers"];
+        $result['message'] = $this->message;
+        $result['errors'] = $this->errors;
+        return $result;
+    }
+
+    /**
+     * Метод подготовки основной части страницы
      * @param string $testTheme - тема теста
      */
     protected function prepareBody($testTheme)
     {
-        $number = rand(1,self::getThemeList()[$testTheme]['count']);
-        echo "Всего вопросов - ". self::getThemeList()[$testTheme]['count'] . " текущий - ".$number;
-        $random = false; // будем получать из метода получения пользовательских настроек
-        $test = $this->getTest($testTheme,$number,$random);
+        $template = $this->separation();
+        $execute = 'build'.ucfirst($template);
+        $content = $this->$execute();
+        foreach ($content as $key=>$item){
+            $$key = $item;
+        }
+        //$_SESSION["curent"] = [];
+        include VIEW_DIR_TEST.$template.'.php';
+        echo "<pre>";
+        //echo json_encode($content);
+        print_r($content);
+        echo "</pre>";
 
-        if(empty($test)){
-            include VIEW_DIR_TEST."notest.php";
-        }
-        elseif(isset($_POST[$testTheme])) {
-            if ($this->checkAnswer($testTheme))
-                echo "Верно!";
-            else
-                echo "Ошибка!";
-            $theme = $_SESSION["curent"]["theme"];
-            //$_SESSION["curent"]["code"] = $code = $testTheme;
-            $num = $_SESSION["curent"]["number"];
-            $quest = $_SESSION["curent"]["quest"];
-            $answers = $_SESSION["curent"]["answers"];
-            include VIEW_DIR_TEST."wrong.php";
-        }
-        else{
-            $_SESSION["curent"] = $test;
-            $theme = $test["theme"];
-            $_SESSION["curent"]["code"] = $code = $testTheme;
-            $num = $test["number"];
-            $quest = $test["quest"];
-            $answers = $test["answers"];
-            include VIEW_DIR_TEST."test.php";
-        }
         include VIEW_DIR_TEST."footer.php";
-
     }
 
     /**
@@ -71,24 +146,36 @@ class Test extends Page
      * @param bool $rnd
      * @return array|bool
      */
-    protected function getTest($theme,$number,$rnd = false){
-        $query = "SELECT * from quest INNER JOIN theme ON (quest.theme_code = theme.theme_code)
-                  INNER JOIN answ ON (quest.quest_id = answ.quest_id) WHERE theme.theme_code = ? AND quest_number = ?";
-        $tests = DBase::select($query,[$theme,$number]);
+    public function getTest($theme,$number = false,$rnd = false){
+        if($number === false)
+            $where = " > 0";
+        else
+            $where = " = ?";
+        $query = "SELECT * from quest INNER JOIN theme ON ".
+            "(quest.theme_code = theme.theme_code)".
+            "INNER JOIN answ ON (quest.quest_id = answ.quest_id) ".
+            "WHERE theme.theme_code = ? AND quest_number" . $where;
+        if($number === false)
+            $tests = DBase::select($query,[$theme]);
+        else
+            $tests = DBase::select($query,[$theme,$number]);
         if(empty($tests)) return false;
         $result =[];
         foreach ($tests as $key => $item){
-            if(!isset($result['number'])) $result['number'] = $item['quest_number'];
-            if(!isset($result['quest'])) $result['quest'] = $item['quest_text'];
-            if(!isset($result['theme'])) $result['theme'] = $item['theme_text'];
-            $result['answers'][] = [
+            if(!isset($result['number'])) $result[$item['quest_number']]['number'] = $item['quest_number'];
+            if(!isset($result['quest'])) $result[$item['quest_number']]['quest'] = $item['quest_text'];
+            if(!isset($result['theme'])) $result[$item['quest_number']]['theme'] = $item['theme_text'];
+            $result[$item['quest_number']]['answers'][] = [
                 'order'=> $item['answ_order'],
                 'right' => $item['answ_right'],
                 'text' => $item['answ_text']
             ];
 
-            if($item['answ_right'] == 1) $result['right'][] = $item['answ_order'];
+            if($item['answ_right'] == 1) $result[$item['quest_number']]['right'][] = $item['answ_order'];
 
+        }
+        if($number !== false){
+            $result = $result[$item['quest_number']];
         }
         if($rnd) shuffle($result['answers']);
         return $result;
