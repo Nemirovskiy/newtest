a6bb257b820638853204efbe806e2197407a1b96
Николай <it-nikola@mail.ru>
2018-03-23 20:33:07 +0300

    переделка под бутстрап, изменение дизайна и логики интерфейса

--- view/admin/addtest.php
+++ view/admin/addtest.php
@@ -3,58 +3,149 @@
  * Страница добавления тестов в базу
  */
 ?>
-<style>
-    input[type="radio"] + input[type="radio"] {
-        display: none;
-    }
-</style>
-<div class='cntr'>
-<h1>Добавление тестов</h1>
-    <? if(!empty($message)): ?>
-        <h4 style="color: blue;"><?=$message?></h4>
-    <?endif;?>
-    <form method="POST" enctype="multipart/form-data" style="text-align: left;">
-        <label>
-            <input checked type="radio" id="theme" value="new">
-            <input type="text" placeholder="new" name="code[]">
-            <input type="text" placeholder="Новая" name="name[]">
-        </label><br>
-            <?
-                foreach ($theme as $item):
-            ?>
-                    <label>
-                        <input type="radio" name="code[]" value="<?=$item['code']?>">
-                        <input type="radio" name="name[]" value="<?=$item['text']?>">
-                        <?=$item['code']?> - <?=$item['text']?>
-                    </label><br>
-            <?
-                endforeach;
-            ?>
-        </input>
-        <input type="file" name="file">
-        <textarea name="text"></textarea><br>
-        <input type="submit" value="Добавить тест"><br>
-        <input type="submit" value="Просмотреть тесты">
-    </form>
+
+<? if(!empty($message)): ?>
+    <h4 style="color: blue;"><?=$message?></h4>
+<?endif;?>
+<? if(!empty($errors)): ?>
+    <div class="modal fade" id="errors" tabindex="-1"
+         role="dialog" aria-labelledby="errors" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLongTitle">Ошибка</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body alert alert-danger" role="alert">
+                    <?=$errors?>
+                </div>
+            </div>
+        </div>
+    </div>
+    <h4 style="color: red;"></h4>
+    <script>
+        $('#errors').modal();
+    </script>
+<?endif;?>
+<ul class="nav nav-tabs" id="myTab" role="tablist">
+    <li class="nav-item">
+        <a class="nav-link active" id="home-tab" data-toggle="tab" href="#addtest" role="tab" aria-controls="home" aria-selected="true">Добавление тестов</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Profile</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" id="contact-tab" data-toggle="tab" href="#contact" role="tab" aria-controls="contact" aria-selected="false">Contact</a>
+    </li>
+</ul>
+<div class="tab-content" id="myTabContent">
+    <div class="tab-pane fade show active" id="addtest" role="tabpanel" aria-labelledby="home-tab">
+        <h1>Добавление тестов</h1>
+        <form method="POST" enctype="multipart/form-data" id="addForm">
+            <div class="form-group row">
+                <label class="col-sm-2 col-form-label" for="inputTheme">Тема</label>
+                <select id="inputTheme" name="addCode" class="form-control col-sm-3">
+                    <option selected value="new">Новая</option>
+                    <? foreach ($theme as $item): ?>
+                        <option value="<?=$item['code']?>"><?=$item['text']?></option>
+                    <? endforeach; ?>
+                </select>
+                <div id="newTheme" class="col-sm-7 row">
+                    <div class="col-sm-5 col">
+                        <input type="text" class="form-control" name="newCode" id="newThemeCode" placeholder="код темы">
+                    </div>
+                    <div class="col-sm-7 col">
+                        <input type="text" class="form-control" name="newName" id="newThemeName" placeholder="Название темы">
+                    </div>
+                </div>
+                <div class="col-sm-12">
+                    <p class="textNewTheme mb-1 text-center" id="textNewTeme"></p>
+                </div>
+                <div class="col-sm-8 row mt-0 mb-2 mx-auto">
+                    <div class="col-sm">
+                        <label class="btn btn-outline-primary" for="buttonFile">Выберите файл</label>
+                        <input style="display: none;" type="file" name="file"  accept="text/plain" id="buttonFile">
+                    </div>
+                    <div class="col-sm">
+                        <input type="button" id="buttonText" name="onText" class="btn btn-outline-primary" value="Добавить текст">
+                    </div>
+                </div>
+                <div class="col-sm-12">
+                    <p class="textNewTheme mb-1 text-center" id="textFileTeme"></p>
+                    <textarea disabled style="display: none" style="overflow-x: hidden; overflow-y: visible;" name="text" id="textArea" class="form-control" ></textarea>
+                </div>
+                <div class="col-sm-10 row  mx-auto mt-2">
+                    <input type="submit" id="buttonAddTest" name="addTest"
+                           class="btn btn-outline-primary col-sm-12" value="Добавить тест">
+                </div>
+            </div>
+        </form>
+
+    </div>
+    <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">...</div>
+    <div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">...</div>
+</div>
+
     <script>
         $(function () {
-            $('#theme').parent().on('click',function () {
-                $(':checked').prop("checked",false);
-                $("#theme").prop("checked",true);
-                $("input[type='text']").removeProp('disabled');
+            $("#inputTheme").on("input",function () {
+                if($(this).val() === 'new'){
+                    $('#newTheme').show(500);
+                    $('#newTheme input').prop('disabled',false);
+                    $('#textNewTeme').text('');
+                }
+                else {
+                    $('#newTheme').hide(500);
+                    $('#newTheme input').prop('disabled',true);
+                    $('#textNewTeme').text('Будет обновлена тема '+$("#inputTheme option:selected").text());
+                }
             });
-            $("input[type='text']").on("input",function () {
-                if($(this).val().length > 0){
-                    $(":radio").prop('disabled',true);
+            $('#newTheme').on('input','input',function () {
+                var name = $('#newThemeName').val();
+                var code = $('#newThemeCode').val();
+                if(name.length > 0 && code.length > 0 )
+                    $('#textNewTeme').text('Будет создана тема с кодом: '+
+                        code + ' названием: ' + name);
+                else
+                    $('#textNewTeme').text('');
+            });
+            $('#buttonText').on('click',function () {
+                $('#textArea').toggle();
+                if($('#textArea').is(':hidden')){
+                    $('#textArea').prop('disabled',true);
+                    $('#buttonText').val('Добавить текст');
+                    $('#buttonFile').prop('disabled',false);
+                    $('[for=\'buttonFile\']').removeClass('disabled');
                 }
                 else{
-                    $(":radio").prop("disabled",false);
+                    $('#textArea').prop('disabled',false);
+                    $('#buttonText').val('Убрать текст');
+                    $('#buttonFile').prop('disabled',true);
+                    $('[for=\'buttonFile\']').addClass('disabled');
+                    $('#textFileTeme').text('');
                 }
             });
-            $(":radio").on("click",function () {
-                $("#theme").prop("checked",false);
-                $("input[type='text']").prop("disabled",true);
-                $(this).next().prop("checked",true);
+            $('#buttonFile').on('change',function () {
+                $('#textFileTeme').text('Вставим тесты из файла ' + $(this).val().split("\\").pop());
+            });
+            $('#addForm').on('submit',function (e) {
+                var flag = true;
+                $('#addForm input, #addForm select, #addForm textarea')
+                    .not(':disabled, [type=\'submit\'], [type=\'submit\']').each(function () {
+                    if($(this).val().length < 1){
+                        $(this).addClass('is-invalid');
+                        $(this).prev('label').addClass('invalid-feedback');
+                    }else{
+                        $(this).removeClass('is-invalid');
+                        $(this).prev('label').removeClass('invalid-feedback');
+                    }
+                    if($(this).val().length < 1 && flag)
+                        flag = false;
+                })
+                if(!flag)
+                e.preventDefault();
             })
         })
     </script>
