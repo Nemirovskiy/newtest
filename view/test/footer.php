ae20bb81c420033cc7dd5a564753222f649284f8
Николай <it-nikola@mail.ru>
2018-03-28 15:03:24 +0300

    выключение нижних кнопок

--- view/test/footer.php
+++ view/test/footer.php
@@ -1,38 +1,38 @@
 <div class='cntr'><br>
-    <form action="post" method="post">
-        <input class="btn m-2 col-sm-2 btn-outline-warning btn-sm" type="submit" name="reset" value="Сброс">
+    <form method="post">
+        <button class="btn m-2 col-sm-2 col-3 btn-outline-warning btn-sm"  name="reset" value="<?=$code?>">Сброс</button>
     </form>
     <br>
-    <form action="post"  method="post">
-        <ul class='sett'>
-            <li>Темная тема<br>
-                <label>
-                    <input type="submit" name="bg" value="black" style="display: none;">
-                    <span class='btn_rdn'>
-				  <span class="noactive"></span></span>
-                </label>
-            </li>
-            <li>Ответы cлучайно<br>
-                <label>
-                    <input type="submit" name="rnd" value="no" style="display: none;">
-                    <span class='btn_rdn'>
-				  <span class="noactive"></span></span>
-                </label>
-            </li>
-            <li>Вопросы бесконечно<br>
-                <label>
-                    <input type="submit" name="infinity" value="yes" style="display: none;">
-                    <span class='btn_rdn'>
-				  <span class="active"></span></span>
-                </label>
-            </li>
-        </ul>
-    </form>
-    <br><br><br><a   href='<? echo $_GET['t'].'-test.txt';?>'>[Скачать тест]</a>
-    <a   href='<? echo $_GET['t'].'-test-otv.txt';?>'>[Скачать ответы]</a><br>
-    <br>
-    Сайт сделан на некомерческой основе.<br>
-    Если сайт Вам принес пользу можете<br>
-    <a href='.?donate'>поддержать автора материально</a>
-    <br>
+<!--    <form action="post"  method="post">-->
+<!--        <ul class='sett'>-->
+<!--            <li>Темная тема<br>-->
+<!--                <label>-->
+<!--                    <input type="submit" name="bg" value="black" style="display: none;">-->
+<!--                    <span class='btn_rdn'>-->
+<!--				  <span class="noactive"></span></span>-->
+<!--                </label>-->
+<!--            </li>-->
+<!--            <li>Ответы cлучайно<br>-->
+<!--                <label>-->
+<!--                    <input type="submit" name="rnd" value="no" style="display: none;">-->
+<!--                    <span class='btn_rdn'>-->
+<!--				  <span class="noactive"></span></span>-->
+<!--                </label>-->
+<!--            </li>-->
+<!--            <li>Вопросы бесконечно<br>-->
+<!--                <label>-->
+<!--                    <input type="submit" name="infinity" value="yes" style="display: none;">-->
+<!--                    <span class='btn_rdn'>-->
+<!--				  <span class="active"></span></span>-->
+<!--                </label>-->
+<!--            </li>-->
+<!--        </ul>-->
+<!--    </form>-->
+<!--    <br><br><br><a   href='--><?// echo $_GET['t'].'-test.txt';?><!--'>[Скачать тест]</a>-->
+<!--    <a   href='--><?// echo $_GET['t'].'-test-otv.txt';?><!--'>[Скачать ответы]</a><br>-->
+<!--    <br>-->
+<!--    Сайт сделан на некомерческой основе.<br>-->
+<!--    Если сайт Вам принес пользу можете<br>-->
+<!--    <a href='.?donate'>поддержать автора материально</a>-->
+<!--    <br>-->
 </div>
