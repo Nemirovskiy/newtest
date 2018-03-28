b13c954a9997acff601e6e61425d52c34d67a23f
Николай <it-nikola@mail.ru>
2018-03-28 14:58:31 +0300

    верстка (добавлен класс для списка, добавлены классы для раскрасски списка, изменение размеров некоторых колонок) и переименование кнопок

--- view/admin/addtest_preview.php
+++ view/admin/addtest_preview.php
@@ -7,23 +7,23 @@
 <? if(!empty($message)): ?>
     <h4 style="color: blue;"><?=$message?></h4>
 <?endif;?>
-<ul>
+<ul class="list">
     <?if(is_array($addTests)) foreach ($addTests as $quest):?>
         <li class="quest">
             <b class="quest-number">№ <?=$quest['number']?> </b>
             <span class="quest-text"> <?=$quest['quest']?></span></li>
         <? foreach($quest['answers'] as $answer): ?>
-            <li class="<?if($answer['right']) echo "r_otv"?>" >
+            <li class="<?if($answer['right']) echo "text-success"?>" >
                 <?=$answer['text']?>
             </li>
         <? endforeach;?>
     <? endforeach; ?>
 </ul>
-<form class="col-sm-8 row mx-auto justify-content-center" method="POST">
+<form class="col-sm-12 row mx-auto text-center justify-content-center" method="POST">
     <? if(empty($errors)): ?>
-        <button class="btn m-2 col-sm-5 btn-success" name="submit">Добавить</button>
+        <button class="btn m-2 col-sm  btn-success" name="submit">Добавить ?? вопросов</button>
     <?endif;?>
-    <button class="btn m-2 col-sm-5 btn-warning" name="reset">Изменить</button>
+    <button class="btn m-2 col-sm btn-warning" name="reset">Отмена</button>
 
 </form>
 
