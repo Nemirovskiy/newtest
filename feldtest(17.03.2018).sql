b5fc03225ad5ebd5b5116eac634bbae3787c12b2
Николай <it-nikola@mail.ru>
2018-09-11 18:27:52 +0300

    актуальный дамп БД

--- feldtest(17.03.2018).sql
+++ feldtest(17.03.2018).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Мар 17 2018 г., 08:39
+-- Время создания: Сен 17 2018 г., 08:39
 -- Версия сервера: 5.7.20
 -- Версия PHP: 5.6.32
 
@@ -21,6 +21,8 @@ SET time_zone = "+00:00";
 --
 -- База данных: `feldtest`
 --
+CREATE DATABASE IF NOT EXISTS `feldtest` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `feldtest`;
 
 -- --------------------------------------------------------
 
@@ -28,10 +30,12 @@ SET time_zone = "+00:00";
 -- Структура таблицы `access`
 --
 
-CREATE TABLE `access` (
-  `id` int(11) NOT NULL,
-  `name` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE IF NOT EXISTS `access` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 
 --
 -- Дамп данных таблицы `access`
@@ -49,13 +53,14 @@ INSERT INTO `access` (`id`, `name`) VALUES
 -- Структура таблицы `answ`
 --
 
-CREATE TABLE `answ` (
-  `answ_id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `answ` (
+  `answ_id` int(11) NOT NULL AUTO_INCREMENT,
   `quest_id` int(11) NOT NULL,
   `answ_order` int(11) NOT NULL,
   `answ_right` tinyint(1) NOT NULL DEFAULT '0',
-  `answ_text` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `answ_text` text NOT NULL,
+  PRIMARY KEY (`answ_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
 --
 -- Дамп данных таблицы `answ`
@@ -72,12 +77,13 @@ INSERT INTO `answ` (`answ_id`, `quest_id`, `answ_order`, `answ_right`, `answ_tex
 -- Структура таблицы `page`
 --
 
-CREATE TABLE `page` (
+CREATE TABLE IF NOT EXISTS `page` (
   `code` varchar(20) NOT NULL,
   `menu` tinyint(1) DEFAULT NULL,
   `admin_menu` tinyint(1) DEFAULT NULL,
   `title` varchar(20) NOT NULL,
-  `access` tinyint(1) NOT NULL DEFAULT '1'
+  `access` tinyint(1) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`code`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -100,12 +106,13 @@ INSERT INTO `page` (`code`, `menu`, `admin_menu`, `title`, `access`) VALUES
 -- Структура таблицы `quest`
 --
 
-CREATE TABLE `quest` (
-  `quest_id` tinyint(4) NOT NULL,
+CREATE TABLE IF NOT EXISTS `quest` (
+  `quest_id` tinyint(4) NOT NULL AUTO_INCREMENT,
   `theme_code` varchar(10) NOT NULL,
   `quest_number` int(11) NOT NULL,
-  `quest_text` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `quest_text` text NOT NULL,
+  PRIMARY KEY (`quest_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 
 --
 -- Дамп данных таблицы `quest`
@@ -123,9 +130,11 @@ INSERT INTO `quest` (`quest_id`, `theme_code`, `quest_number`, `quest_text`) VAL
 -- Структура таблицы `theme`
 --
 
-CREATE TABLE `theme` (
+CREATE TABLE IF NOT EXISTS `theme` (
   `theme_code` varchar(10) NOT NULL,
-  `theme_text` varchar(20) NOT NULL
+  `theme_text` varchar(20) NOT NULL,
+  PRIMARY KEY (`theme_code`),
+  UNIQUE KEY `theme_code` (`theme_code`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -137,64 +146,21 @@ INSERT INTO `theme` (`theme_code`, `theme_text`) VALUES
 ('ser', 'СанЭпидРежим'),
 ('smp', 'Скорая помощь');
 
---
--- Индексы сохранённых таблиц
---
+-- --------------------------------------------------------
 
 --
--- Индексы таблицы `access`
---
-ALTER TABLE `access`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `id` (`id`);
-
---
--- Индексы таблицы `answ`
---
-ALTER TABLE `answ`
-  ADD PRIMARY KEY (`answ_id`);
-
---
--- Индексы таблицы `page`
---
-ALTER TABLE `page`
-  ADD PRIMARY KEY (`code`);
-
---
--- Индексы таблицы `quest`
---
-ALTER TABLE `quest`
-  ADD PRIMARY KEY (`quest_id`);
-
---
--- Индексы таблицы `theme`
---
-ALTER TABLE `theme`
-  ADD PRIMARY KEY (`theme_code`),
-  ADD UNIQUE KEY `theme_code` (`theme_code`);
-
---
--- AUTO_INCREMENT для сохранённых таблиц
+-- Структура таблицы `user`
 --
 
---
--- AUTO_INCREMENT для таблицы `access`
---
-ALTER TABLE `access`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
---
--- AUTO_INCREMENT для таблицы `answ`
---
-ALTER TABLE `answ`
-  MODIFY `answ_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT для таблицы `quest`
---
-ALTER TABLE `quest`
-  MODIFY `quest_id` tinyint(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-COMMIT;
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `login` varchar(255) NOT NULL,
+  `pass` varchar(255) NOT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `access` tinyint(1) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `login` (`login`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
